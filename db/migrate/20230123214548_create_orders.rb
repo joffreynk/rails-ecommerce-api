335cb085fdda6653ac0c1aba78dce3c973b8f09a
Joffreynk <joyjoffrey@gmail.com>
2023-01-23 23:46:08 +0200

    create order  model

---
+++ db/migrate/20230123214548_create_orders.rb
@@ -0,0 +1,13 @@
+class CreateOrders < ActiveRecord::Migration[7.0]
+  def change
+    create_table :orders do |t|
+      t.integer :numberOfItems
+      t.decimal :totalPrice
+      t.string :shippingLocation
+      t.references :user, null: false, foreign_key: true
+      t.references :product, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
