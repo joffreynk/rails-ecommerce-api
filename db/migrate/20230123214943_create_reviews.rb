f191021404adcd193d63834e9db23242c251e79b
Joffreynk <joyjoffrey@gmail.com>
2023-01-23 23:50:08 +0200

    create review  model

---
+++ db/migrate/20230123214943_create_reviews.rb
@@ -0,0 +1,13 @@
+class CreateReviews < ActiveRecord::Migration[7.0]
+  def change
+    create_table :reviews do |t|
+      t.decimal :rating
+      t.text :message
+      t.boolean :status
+      t.references :user, null: false, foreign_key: true
+      t.references :product, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
