6c16175e202c088605941a1896e5948cfe363f6d
Joffreynk <joyjoffrey@gmail.com>
2023-01-23 22:31:21 +0200

    create user  model

---
+++ db/migrate/20230123203033_create_users.rb
@@ -0,0 +1,16 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :email
+      t.string :password
+      t.string :firstName
+      t.string :lastName
+      t.string :dob
+      t.string :phoneNumber
+      t.string :address
+      t.boolean :isAdmin, default: false
+
+      t.timestamps
+    end
+  end
+end
