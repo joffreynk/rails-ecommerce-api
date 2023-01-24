bb3ed339f07c749a6a9abe6bf522ac514b98e424
Joffreynk <joyjoffrey@gmail.com>
2023-01-24 08:58:06 +0200

    change from funtion reference to call

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -3,8 +3,20 @@ class Api::V1::CategoriesController < ApplicationController
     render json: Category.all
   end
 
+  def show
+    render json: Category.find(params[:id])
+  end
+
+  def create
+    @category = Category.new(catagory_params())
+    if @category.save
+      render json: @category, status: :created
+    end
+  end
+
   private
   def catagory_params
     params.require(:category).permit(:name, :description);
   end
+
 end
