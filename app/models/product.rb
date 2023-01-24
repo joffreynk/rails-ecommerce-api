1393f5b373219a2923cfe04dc89b5eea5819497f
Joffreynk <joyjoffrey@gmail.com>
2023-01-24 18:00:30 +0200

    validate product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,9 @@
 class Product < ApplicationRecord
   belongs_to :user
   belongs_to :category
+  has_many :reviews
+
+  validates :name, :description, :price, presence:true
+  validates :name, length:{ minimum: 5}
+  validates :price, numeracity:true,
 end
