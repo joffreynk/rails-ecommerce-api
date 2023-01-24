56732afbe944ff1ff4d1c0160e785a31de0b0e24
Joffreynk <joyjoffrey@gmail.com>
2023-01-24 16:52:05 +0200

    validate user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  has_many: products
+  has_many :reviews
+  has_many :orders
+
+  validates :firstName, :lastName, :email, :password, :phoneNumber, :address, presence: true, 
+  validates :password, :phoneNumber, length :{in: 6..15}, 
+  validates :email, presence: true
 end
