1a6fdb5c3c83cd13269e0b34ffb16a3ed081ea63
Joffreynk <joyjoffrey@gmail.com>
2023-01-24 18:05:27 +0200

    create validation for order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,8 @@
 class Order < ApplicationRecord
   belongs_to :user
   belongs_to :product
+
+  validates :shippingLocation, :numberOfItems, :totalPrice, presence:true
+  validates :totalPrice, numeracity:true,
+  validates :numberOfItems, numericality => { :only_integer => true }
 end
