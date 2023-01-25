d3b062b3e4026ec3fa62a59618a330c8df8f5581
Joffreynk <joyjoffrey@gmail.com>
2023-01-25 21:18:19 +0200

    finalize to create user controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -9,15 +9,43 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def create
-  @user = User.new(user_params)
-  if @user.save
+    @user = User.new(user_params)
+    if @user.save
 
-    token = JsonWebToken.encode(user_id: @user.id)
-    time = Time.now + 24.hours.to_i
-    render json: { token:, exp: time.strftime('%m-%d-%Y %H:%M'),
-                    name: @user.name, user_name: @user.user_name, role: @user.role }, status: 200
-  else
-    render json: { errors: @user.errors.full_messages }, status: 503
+      token = JsonWebToken.encode(user_id: @user.id)
+      time = Time.now + 24.hours.to_i
+      render json: { token:, exp: time.strftime('%m-%d-%Y %H:%M'),
+                      name: @user.name, user_name: @user.user_name, role: @user.role }, status: 200
+    else
+      render json: { errors: @user.errors.full_messages }, status: 503
+    end
   end
+
+  def show
+    render json: find_user, status: 200
+  end
+
+  def update
+    if find_user.update(user_params)
+      render json: {m: 'user updated successfully'}: status: 201
+    else
+      render json: {errors: find_user.errors.full_messages}, status: 503
+    end
+  end
+
+  def destroy
+    if find_user.destroy
+      render json: { m: 'user deleted successfully' }, status: 200
+    else
+      render json: { errors: find_user.errors.full_messages }, status: 503
+    end
+
+  private
+  def find_user
+    User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:firstName, :lastName, :email, :password, :phoneNumber, :address: password)
   end
 end
