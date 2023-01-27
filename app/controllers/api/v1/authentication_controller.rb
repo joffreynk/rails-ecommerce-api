29c1d6e09068073dac3ea0805484d24721af96ff
Joffreynk <joyjoffrey@gmail.com>
2023-01-27 22:01:10 +0200

    add get orders method

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -4,12 +4,12 @@ class Api::V1::AuthController < ApplicationController
 
     # POST /auth/login
     def login
-      @user = User.find_by(user_name: params[:user_name])
-      if @user&.authenticate(params[:password])
-        token = JsonWebToken.encode(user_id: @user.id)
+      user = User.find_by(user_name: params[:user_name])
+      if user&.authenticate(params[:password])
+        token = JsonWebToken.encode(user_id: user.id)
         time = Time.now + 24.hours.to_i
         render json: { token:, exp: time.strftime('%m-%d-%Y %H:%M'),
-                        name: @user.name, user_name: @user.user_name, role: @user.role }, status: :ok
+                        name: user.name, user_name: user.user_name, role: user.role }, status: :ok
       else
         render json: { error: 'unauthorized user, please register or contact your admin' }, status: :unauthorized
       end
--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -1,2 +1,19 @@
 class Api::V1::OrdersController < ApplicationController
+  before_action :authenticate_user
+
+  def index
+    if @current_user.isAdmin
+      render json: Order.all, status: :ok
+    end
+    if !@current_user.isAdmin
+      render json: find_orders_by_user_id, status: :ok
+    end
+    render json: {error: 'ooops, no orders was made'}, status: 404
+  end
+
+  private
+  def find_orders_by_user_id
+    Order.find(@current_user.id)
+  end
+  
 end
