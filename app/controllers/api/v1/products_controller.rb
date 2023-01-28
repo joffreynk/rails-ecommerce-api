27259a13cd2001407f715152975cee0d62b2dfbb
Joffreynk <joyjoffrey@gmail.com>
2023-01-28 22:11:17 +0200

    add create product method

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -3,13 +3,25 @@ class Api::V1::ProductsController < ApplicationController
 
     # GET /api/v1/products
     def index
-        @products = Product.all
-        render json: @products, status: :ok
+        render json: Product.all, status: :ok
+    end
+
+    def create
+        if @current_user.isAdmin
+            product = Product.new(user_id: @current_user.id, *product_params)
+            if product.save
+                render json: product, status: 201
+            else
+                render json: product.errors, status: 401
+            end
+        else
+            render json: {error: 'you are not allowed to create a product, please contact your administrator'}, status: 401
+        end
     end
 
 
     private
     def product_params
-        params.require(:product).permit(:name, :description, :price, :category_id, :user_id)
+        params.require(:product).permit(:name, :description, :price, :category_id)
     end
 end
