892903c18f659cdf0648e76400887305ce3604e6
Joffreynk <joyjoffrey@gmail.com>
2023-01-31 00:56:12 +0200

    create product pictureurl

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,4 +7,8 @@ class Product < ApplicationRecord
   validates :name, :description, :price, :category_id, :user_id, presence: true
   validates :name, length:{ minimum: 5}
   validates :price, numericality: true
+
+  def product_picture_ul
+    Rails.application.routes.url_helpers.url_for(product_picture) if product_picture.attached?
+  end
 end
