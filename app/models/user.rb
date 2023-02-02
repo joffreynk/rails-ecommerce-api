47e261114a3ed1935075c612d69af747c7efb6ed
Joffreynk <joyjoffrey@gmail.com>
2023-02-02 19:54:08 +0200

    crate picture url

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_one_attached :profile_picture
   has_many :products
   has_many :reviews
   has_many :orders
@@ -6,4 +7,8 @@ class User < ApplicationRecord
   validates :firstName, :lastName, :email, :password_digest, :phoneNumber, :address, presence: true 
   validates :password_digest, :phoneNumber, length: {in: 6..15}
   validates :email, uniqueness: true
+
+  def profile_picture_url
+    Rails.application.routes.url_helpers.url_for(profile_picture) if profile_picture.attached
+  end
 end
